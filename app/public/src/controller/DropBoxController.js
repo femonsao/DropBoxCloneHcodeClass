3b36566afb2d0ed80a13bf8a862586e96d920ecc
Felipe Monsão <femonsao@gmail.com>
2020-12-09 11:02:40 -0300

    criando upload

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -25,13 +25,67 @@ class DropBoxController {
 
         this.inputFilesEl.addEventListener('change', event => {
 
-            console.log(event.target.files);
+            console.log('calling uploadFiles function');
+            this.uploadTask(event.target.files);
 
             this.modalFileEL.style.display = 'block'
 
 
         });
 
+
+
+
+    }
+
+    uploadTask(files) {
+        //console.log(typeof(files), files, 'files in function');
+
+        let promises = [];
+
+        [...files].forEach(file => {
+            //console.log(typeof(file), file, 'file in loop');
+            promises.push(new Promise((resolve, reject) => {
+
+
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+
+
+                    } catch (e) {
+                        reject(e);
+                    }
+                }
+
+                ajax.onerror = event => {
+
+                    reject(event);
+
+
+                }
+
+                let formData = new FormData();
+
+                console.log(file, "file to form");
+
+                formData.append('input-file', file);
+
+                console.log(formData, 'form ')
+
+                ajax.send(formData);
+
+            }));
+
+            //console.log(typeof(promises), promises, 'promise in loop');
+        })
+
+        return Promise.all(promises);
 
 
 
